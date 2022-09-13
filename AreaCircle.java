45b4d2b6f56376eabd54e0bec8d87a2cfce295cc
Kaustubh madhav jadhav <63606341+Champion028K@users.noreply.github.com>
2022-09-13 21:20:19 +0530

    First commit

---
+++ AreaCircle.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.Math;
+class AreaCircle
+ {
+    public static void main(String a[])
+    {
+        float pi = (float)3.14;
+        float r = 5;
+        float ans ;
+        ans = pi*(r*r);
+        System.out.println("Area of circle "+ ans +" Radius is "+ r);
+
+    }
+}
